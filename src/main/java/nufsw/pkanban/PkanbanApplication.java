0636f680688d8221a182918c106b1964dc75aa7a
Siqueira <andre.ruela@marinha.mil.br>
2020-11-20 12:54:23 -0300

    initial commit

---
+++ src/main/java/nufsw/pkanban/PkanbanApplication.java
@@ -0,0 +1,13 @@
+package nufsw.pkanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkanbanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nufsw/pkanban/PkanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package nufsw.pkanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
